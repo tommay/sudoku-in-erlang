59871fcdb3918b7762128a58bf97410738551cb0
Tom May <tom@tommay.net>
2015-02-16 22:53:33 -0800

    Split sudoku.erl back out of sudoku so stuff can be called from the shell.

---
+++ sudoku.erl
@@ -0,0 +1,23 @@
+-module(sudoku).
+-export([start/1]).
+
+%% Initializes Puzzle from the given Filename and prints out solutions
+%% if any.
+%%
+start(Filename) ->
+    Setup = get_setup(Filename),
+    Puzzle = puzzle:new(Setup),
+    Solutions = puzzle:solve(Puzzle),
+    lists:foreach(
+      fun (SolvedPuzzle) ->
+	      io:format("~n"),
+	      puzzle:print_puzzle(SolvedPuzzle),
+	      io:format("~n")
+      end,
+      Solutions),
+    io:format("~w solutions~n", [length(Solutions)]).
+
+get_setup(Filename) ->
+    {ok, Raw} = file:read_file(Filename),
+    NoComments = re:replace(Raw, "#.*", "", [global, {return, list}]),
+    re:replace(NoComments, "\\s+", "", [global, {return, list}]).
